b4ed4fe4bdeb3934cd0d8d48bbff9406d74981ba
Anatolii <silversklad@gmail.com>
2023-09-11 16:46:46 +0000

    Add new logo

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ new Swiper('.image-slider', {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
+  
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
   // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
+  // },
 
   grabCursor: true,
 
@@ -32,5 +32,31 @@ new Swiper('.image-slider', {
 
   spaceBetween: 40,
 
-  slidesPerView: 1,
+  slidesPerView: 1.2,
 })
+
+
+// окрашиваем буквы заголовка
+const textElement = document.querySelector('.logo__text');
+const text = textElement.textContent;
+
+// Генерируем случайный цвет в формате #RRGGBB
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Создаем новый HTML-код с каждой буквой, окрашенной в случайный цвет
+const colorfulText = text.split('').map(letter => {
+    const span = document.createElement('span');
+    span.textContent = letter;
+    span.style.color = getRandomColor();
+    return span.outerHTML;
+}).join('');
+
+// Заменяем содержимое элемента на окрашенный текст
+textElement.innerHTML = colorfulText;
